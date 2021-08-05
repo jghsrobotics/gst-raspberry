618c23e705cd44cdf948327b428bcffc9e9431b0
Diego Contreras <smarttdiego@gmail.com>
2021-08-05 16:30:10 -0700

    Add range for "Quality" setting in config

--- sender.py
+++ sender.py
@@ -25,7 +25,7 @@ height = GetSetting(configFile,     '# Height')
 width = GetSetting(configFile,      '# Width')
 FPS = GetSetting(configFile,        '# FPS')
 bitrate = GetSetting(configFile,    '# Bitrate')
-quality = GetSetting(configFile,    '# Quality')
+quality = GetSetting(configFile,    '# Quality (10 - 40)')
 
 UDPcommand = "raspivid -n -t 0 -w {0} -h {1} -qp {6} -fps {2} --flush -b {3} -o - | gst-launch-1.0 -e -vvvv fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host={4} port={5}"
 TCPcommand = "raspivid -n -t 0 -w {0} -h {1} -qp {6} -fps {2} --flush -b {3} -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host={4} port={5}"
