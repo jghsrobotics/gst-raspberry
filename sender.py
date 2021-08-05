9ad14678e4eb53121ec76d8f2a27c62e611a086a
Desperationis <smarttdiego@gmail.com>
2021-08-05 12:39:50 -0700

    Add 'Quality' setting to sender and config

--- sender.py
+++ sender.py
@@ -25,18 +25,19 @@ height = GetSetting(configFile,     '# Height')
 width = GetSetting(configFile,      '# Width')
 FPS = GetSetting(configFile,        '# FPS')
 bitrate = GetSetting(configFile,    '# Bitrate')
+quality = GetSetting(configFile,    '# Quality')
 
-UDPcommand = "raspivid -n -t 0 -w {0} -h {1} -qp 40 -fps {2} --flush -b {3} -o - | gst-launch-1.0 -e -vvvv fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host={4} port={5}"
-TCPcommand = "raspivid -n -t 0 -w {0} -h {1} -qp 40 -fps {2} --flush -b {3} -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host={4} port={5}"
+UDPcommand = "raspivid -n -t 0 -w {0} -h {1} -qp {6} -fps {2} --flush -b {3} -o - | gst-launch-1.0 -e -vvvv fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host={4} port={5}"
+TCPcommand = "raspivid -n -t 0 -w {0} -h {1} -qp {6} -fps {2} --flush -b {3} -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host={4} port={5}"
 
 
 if protocol == "TCP":
     print("Trying to send through TCP pipe...")
-    os.system(TCPcommand.format(width, height, FPS, bitrate, localIP, port))
+    os.system(TCPcommand.format(width, height, FPS, bitrate, localIP, port, quality))
 
 elif protocol == "UDP":
     print("Trying to send through UDP pipe...")
-    os.system(UDPcommand.format(width, height, FPS, bitrate, remoteIP, port))
+    os.system(UDPcommand.format(width, height, FPS, bitrate, remoteIP, port, quality))
 
 
 
